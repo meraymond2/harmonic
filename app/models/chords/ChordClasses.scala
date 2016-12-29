b899146316b3916f562fb235aa7a4f4f172191e2
meraymond2 <michael.raymond@mobillity.co>
2016-12-29 18:08:28 +0000

    implements a minor key class

--- app/models/chords/ChordClasses.scala
+++ app/models/chords/ChordClasses.scala
@@ -15,7 +15,47 @@ object ChordClasses extends Enumeration {
   majSixChord,
   dimSixChord,
   minSixFour,
-  majSixFour = Value
+  majSixFour,
+  minorSeventh,
+  minorMajorSeventh,
+  dominantSeventh,
+  majorSeventh,
+  diminishedSeventh,
+  halfDimSeventh,
+  minorSeventhFirstInv,
+  minorMajorSeventhFirstInv,
+  dominantSeventhFirstInv,
+  majorSeventhFirstInv,
+  diminishedSeventhFirstInv,
+  halfDimSeventhFirstInv,
+  minorSeventhSecondInv,
+  minorMajorSeventhSecondInv,
+  dominantSeventhSecondInv,
+  majorSeventhSecondInv,
+  diminishedSeventhSecondInv,
+  halfDimSeventhSecondInv,
+  minorSeventhThirdInv,
+  minorMajorSeventhThirdInv,
+  dominantSeventhThirdInv,
+  majorSeventhThirdInv,
+  diminishedSeventhThirdInv,
+  halfDimSeventhThirdInv = Value
 
+  class ChordClassValue(chordClass: ChordClass) {
+    def andInversions: Set[ChordClass] = chordClass match {
+      case triad if triad == minorTriad => Set(minorTriad, minSixChord, minSixFour)
+      case triad if triad == majorTriad => Set(majorTriad, majSixChord, majSixFour)
+      case triad if triad == diminishedTriad => Set(diminishedTriad, dimSixChord)
+      case seventh if seventh == minorSeventh => Set(minorSeventh, minorSeventhFirstInv, minorSeventhSecondInv, minorSeventhThirdInv)
+      case seventh if seventh == minorMajorSeventh => Set(minorMajorSeventh, minorMajorSeventhFirstInv, minorMajorSeventhSecondInv, minorMajorSeventhThirdInv)
+      case seventh if seventh == dominantSeventh => Set(dominantSeventh, dominantSeventhFirstInv, dominantSeventhSecondInv, dominantSeventhThirdInv)
+      case seventh if seventh == majorSeventh => Set(majorSeventh, majorSeventhFirstInv, majorSeventhSecondInv, majorSeventhThirdInv)
+      case seventh if seventh == diminishedSeventh => Set(diminishedSeventh, diminishedSeventhFirstInv, diminishedSeventhSecondInv, diminishedSeventhThirdInv)
+      case seventh if seventh == halfDimSeventh => Set(halfDimSeventh, halfDimSeventhFirstInv, halfDimSeventhSecondInv, halfDimSeventhThirdInv)
+      case _ => Set.empty[ChordClass]
+    }
+  }
+
+  implicit def value2ChordClassValue(chordClass: Value): ChordClassValue = new ChordClassValue(chordClass)
 
 }
--- app/models/keys/MinorKey.scala
+++ app/models/keys/MinorKey.scala
@@ -3,31 +3,32 @@ package models.keys
 import dao.IntervalDb._
 import models.chords.Chord
 import models.chords.ChordClasses._
-import models.notes.Note
 import models.notes.PitchClasses.PitchClass
 
 /**
   * Created by michael on 28/12/16.
   */
 
-class MinorKey (root: Note) extends Key {
+class MinorKey (root: PitchClass) extends Key {
 
   // Diatonic Notes
-  val tonicNote: Note = root
-  lazy val supertonicNote: Note = tonicNote ↑ maj2nd
-  lazy val mediantNote: Note = tonicNote ↑ min3rd
-  lazy val subdominantNote: Note = tonicNote ↑ per4th
-  lazy val dominantNote: Note = tonicNote ↑ per5th
-  lazy val submediantNote: Note = tonicNote ↑ min6th
-  lazy val leadingNote: Note = tonicNote ↑ maj7th
+  val tonic: PitchClass            = root
+  lazy val supertonic: PitchClass  = tonic ↑ maj2nd
+  lazy val mediant: PitchClass     = tonic ↑ min3rd
+  lazy val subdominant: PitchClass = tonic ↑ per4th
+  lazy val dominant: PitchClass    = tonic ↑ per5th
+  lazy val submediant: PitchClass  = tonic ↑ min6th
+  lazy val leading: PitchClass     = tonic ↑ maj7th
 
   // Diatonic Chords
-//  lazy val tonicTriad: Chord = Chord(tonicNote.pitchClass, minorTriad)
-//  lazy val supertonicTriad: Chord = Chord(supertonicNote.pitchClass)
-//  lazy val mediantTriad: Chord =
-//  lazy val subdominantTriad: Chord =
-//  lazy val dominantTriad: Chord =
-//  lazy val submediantTriad: Chord =
-//  lazy val leadingTriad: Chord =
+  lazy val chords: Set[Chord] = Set(
+    (minorTriad.andInversions ++ minorMajorSeventh.andInversions).map(chordClass => Chord(tonic, chordClass)),
+    (diminishedTriad.andInversions ++ halfDimSeventh.andInversions).map(chordClass => Chord(supertonic, chordClass)),
+    Set(Chord(mediant, augmentedTriad)), // Ignoring inversions and seventh chords.
+    (minorTriad.andInversions ++ minorSeventh.andInversions).map(chordClass => Chord(subdominant, chordClass)),
+    (majorTriad.andInversions ++ dominantSeventh.andInversions).map(chordClass => Chord(dominant, chordClass)),
+    (majorTriad.andInversions ++ majorSeventh.andInversions).map(chordClass => Chord(submediant, chordClass)),
+    (diminishedTriad.andInversions ++ diminishedSeventh.andInversions).map(chordClass => Chord(tonic, chordClass))
+  ).flatten
 
 }
