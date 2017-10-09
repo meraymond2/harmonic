fea4db157a175054021c9480d6cb18b8c31cc626
Michael <raymond.michael@gmail.com>
2017-10-09 08:05:52 +0100

    Select partially working

--- js/App.js
+++ js/App.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-const App = () => <p>hello</p>;
+const array = ["one", "two", "three"];
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        console.log(event);
+    }
+
+    render() {
+        return (
+            <select size={array.length} multiple value={[]} onChange={this.handleChange}>
+                {array.map((opt) => <option value={opt}>{opt}</option>)}
+            </select>
+        );
+    }
+}
 
 export default App;
