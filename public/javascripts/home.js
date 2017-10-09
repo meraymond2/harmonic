17e88a74615e274cd9a5e082efc4e14eaf12bd42
Michael <raymond.michael@gmail.com>
2017-10-09 07:45:52 +0100

    clear notes on submit

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -12,14 +12,13 @@ const chunk = (arr, len) => {
   }
 
   return chunks;
-}
+};
 
 fetch("/api/notes").then((res) => res.json()).then((json) => {
     notes = json.reduce((acc, note) => {
         acc[note.spn] = note;
         return acc;
     }, {});
-    console.log(json.length)
     const chunked = chunk(json, 17);
     box.innerHTML = chunked.map((octave) =>
         `
@@ -28,7 +27,7 @@ fetch("/api/notes").then((res) => res.json()).then((json) => {
         </select>
         `
     )
-})
+});
 
 const sendNotes = (event) => {
     const selected = document.querySelectorAll('#box option:checked');
@@ -42,7 +41,8 @@ const sendNotes = (event) => {
         method: "POST"
     })
     .then((res => res.json()))
-    .then((json) =>
-        document.getElementById("chord").innerText = JSON.stringify(json, null, 2)
-    );
+    .then((json) => {
+        document.getElementById("chord").innerText = JSON.stringify(json, null, 2);
+        selected.forEach((opt) => opt.selected = false);
+    });
 };
